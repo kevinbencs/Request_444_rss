4ea733dcb1c1cc5dc2b3b84a05cdd65aab090c71
Kevin Bencs <kevinbencs8@gmail.com>
2026-06-26 18:41:16 +0200

    Got the good structure.

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import requests
 import xml.etree.ElementTree as ET
 
 
+namespaces = {
+    "content": "http://purl.org/rss/1.0/modules/content/"
+}
+
 try:
     response = requests.get("https://444.hu/feed",timeout=5)
     response.raise_for_status()
@@ -10,8 +14,15 @@ except requests.exceptions.RequestException as e:
 else:
     root = ET.fromstring(response.content)
     channel = root.find("channel")
-    for item in channel.findall("item")[:5]:  # first 5 articles
+    for item in channel.findall("item")[:5]: 
         title = item.find("title").text
         link = item.find("link").text
+        content_el = item.find("content:encoded", namespaces)
+        full_text = content_el.text if content_el is not None else None
         pub_date = item.find("pubDate").text
-        print(f"{title}\n  {link}\n  {pub_date}\n")
+        print(f"Title: {title}\n")
+        print(f"Link: {link}\n ")
+        print(f"Date: {pub_date}\n")
+        print("---")
+        print(f"Content: {full_text.replace('<p>','').replace('</p>','') if full_text else 'N/A'}...")
+        print("---")
