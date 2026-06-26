79de70bb7c1ad6d95b99b6de4c8c7a08a7b45c2d
Kevin Bencs <kevinbencs8@gmail.com>
2026-06-26 21:52:58 +0200

    Wrote the data into the articlte.txt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
 
 
 namespaces = {
@@ -14,15 +15,18 @@ except requests.exceptions.RequestException as e:
 else:
     root = ET.fromstring(response.content)
     channel = root.find("channel")
-    for item in channel.findall("item")[:5]: 
-        title = item.find("title").text
-        link = item.find("link").text
-        content_el = item.find("content:encoded", namespaces)
-        full_text = content_el.text if content_el is not None else None
-        pub_date = item.find("pubDate").text
-        print(f"Title: {title}\n")
-        print(f"Link: {link}\n ")
-        print(f"Date: {pub_date}\n")
-        print("---")
-        print(f"Content: {full_text.replace('<p>','').replace('</p>','') if full_text else 'N/A'}...")
-        print("---")
+    with open("article.txt", "a", encoding="utf-8") as f:
+        for item in channel.findall("item"): 
+            title = item.find("title").text
+            link = item.find("link").text
+            content_el = item.find("content:encoded", namespaces)
+            full_text = content_el.text if content_el is not None else None
+            clean_text = BeautifulSoup(full_text, "html.parser").get_text() if full_text else "N/A"
+            pub_date = item.find("pubDate").text
+            f.write(f"Title: {title}\n")
+            f.write(f"Link: {link}\n ")
+            f.write(f"Date: {pub_date}\n")
+            f.write("---\n")
+            f.write(f"Content: {clean_text if full_text else 'N/A'}...\n")
+            f.write("---\n \n")
+            print("Saved to article.txt")
