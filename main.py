936ec1ac2c5d8e336d8d79e673d927258fdd8d40
Kevin Bencs <kevinbencs8@gmail.com>
2026-06-27 13:48:04 +0200

    Added requiremnt.txt. Avoided the duplicate article.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import requests
 import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
+import os
 
+seen_links = set()
+seen_file = "article.txt"
 
 namespaces = {
     "content": "http://purl.org/rss/1.0/modules/content/"
 }
+
+if os.path.exists(seen_file):
+    with open(seen_file, "r", encoding="utf-8") as f:
+        seen_links = set(line for line in f)
+
+
 
 try:
     response = requests.get("https://444.hu/feed",timeout=5)
@@ -15,10 +24,14 @@ except requests.exceptions.RequestException as e:
 else:
     root = ET.fromstring(response.content)
     channel = root.find("channel")
-    with open("article.txt", "a", encoding="utf-8") as f:
-        for item in channel.findall("item"): 
-            title = item.find("title").text
+    with open(seen_file, "a", encoding="utf-8") as f:
+        for item in channel.findall("item"):
             link = item.find("link").text
+            if "Link: "+link+"\n" in seen_links:
+                continue
+
+            title = item.find("title").text
+            
             content_el = item.find("content:encoded", namespaces)
             full_text = content_el.text if content_el is not None else None
             clean_text = BeautifulSoup(full_text, "html.parser").get_text() if full_text else "N/A"
